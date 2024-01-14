0ef0acf22169ec9e62a3ab2cc311bb6db1c4ed23
git-tolu <you@example.com>
2024-01-14 22:07:53 +0100

    creating components for thr layout

---
+++ src/components/Class.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import React, { Component } from 'react'
+
+
+class Hello extends React.Component{
+    render(){
+        return(
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default Hello
---
+++ src/components/Functional.js
@@ -0,0 +1,8 @@
+function Heading() {
+    let title = 'this is some heading text';
+    return (
+          <h1> {title}</h1>
+    )
+}
+
+export default Heading
